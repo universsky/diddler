5a1ea2543fff5ab25c7228f2616446e126e41571
Jack Chen <universsky@126.com>
2014-01-22 22:59:51 +0800

    Signed-off-by: Jack Chen <universsky@126.com>

--- src/universsky/diddler/ItemActivity.java
+++ src/universsky/diddler/ItemActivity.java
@@ -3,6 +3,9 @@
  */
 package universsky.diddler;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -17,6 +20,7 @@ public class ItemActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.item);
 		TextView itemTextView = (TextView) findViewById(R.id.itemView1);
+		TextView urlTextView = (TextView) findViewById(R.id.urlView);
 		
 		//Retrieves a map of extended data from the intent.
 		Bundle bundle = getIntent().getExtras();
@@ -28,6 +32,29 @@ public class ItemActivity extends Activity{
 		//Open Declaration void android.widget.TextView.setText(CharSequence text)
 		itemTextView.setText(item);
 		//itemTextView.append(item);
+		
+		String path = "";
+		String host = "";
+		String url = "http://";
+		String itemStr = item.toString();
+		
+		Matcher m1 = Pattern.compile("GET\\s(.*)\\sHTTP/1.1").matcher(itemStr);
+		
+		if (m1.find()){
+			path = itemStr.substring(m1.start(),m1.end());
+			path = path.substring( path.indexOf("GET") + 4, path.indexOf("HTTP/1.1")-1);
+		}
+		
+		Matcher m2 = Pattern.compile("Host:\\s(.*)").matcher(itemStr);
+		if (m2.find()){
+			host = itemStr.substring(m2.start(),m2.end());
+			host = host.substring( host.indexOf("Host:") + 5);
+		}
+		
+		url += host + path;
+		url = url.replaceAll("\\s*|\t|\r|\n", "");
+		urlTextView.setText(url);		
+		
 	}
 
 }
